dedf5cd8e9fd5981903b2e11d0761946294ad1b6
Roman <rk.salimgareev@mail.ru>
2024-09-30 21:32:47 +0300

    add socket client

--- Client_server_chat/myServer.cpp
+++ Client_server_chat/myServer.cpp
@@ -8,20 +8,39 @@
 int main()
 {
 	WSADATA WSData;
-	SOCKET server;
-	SOCKADDR_IN serverAddr;
+	SOCKET server, client;
+	SOCKADDR_IN serverAddr, clientAddr;
 
-	int result = WSAStartup(MAKEWORD(2,2), &WSData);
+	int result = WSAStartup(MAKEWORD(2, 0), &WSData);
 	if (result == 0)
+	{
 		std::cout << "good" << "\n";
+		return 1;
+	}
 
 	server = socket(AF_INET, SOCK_STREAM, 0);
 	if (server != INVALID_SOCKET)
+	{
 		std::cout << "good1" << "\n";
+		return 1;
+	}
 
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_addr.s_addr = INADDR_ANY;
+	serverAddr.sin_port = htons(8000);
+	
+	bind(server, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+	listen(server, 2);
 
+	int clientAddrSize = sizeof(clientAddr);
+	client = accept(server, (SOCKADDR*)&clientAddr, &clientAddrSize);
+	if (client != INVALID_SOCKET)
+	{
+		char buffer[1024] = "Hello client!";
+		send(client, buffer, sizeof(buffer), 0);
+	}
 
+	closesocket(server);
+	closesocket(client);
 	WSACleanup();
 }
